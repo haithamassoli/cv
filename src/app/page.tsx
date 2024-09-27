9406d1689bdc985f96b51f16c5b1fa8af4ef37e3
haithamassoli <haitham.b.assoli@gmail.com>
2024-09-27 23:10:51 +0300

    Update page title to "Haitham's CV"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { RESUME_DATA } from "@/data/resume-data";
 import { ProjectCard } from "@/components/project-card";
 
 export const metadata: Metadata = {
-  title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
+  title: "Haitham's CV",
   description: RESUME_DATA.summary,
 };
 
